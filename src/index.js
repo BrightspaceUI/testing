90cdc57a881bad0dab0d49447fba5d39054f9093
Dave Lockhart <dlockhart@users.noreply.github.com>
2023-06-09 15:08:22 -0400

    re-organize files

--- src/index.js
+++
@@ -1,3 +0,0 @@
-export { aTimeout, defineCE, expect, html, nextFrame, oneEvent, waitUntil } from '@open-wc/testing';
-export { createConfig, getBrowsers } from './server/wtr-config.js';
-export { fixture } from './fixture/fixture.js';
