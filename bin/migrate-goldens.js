6ed7e6f045d0f9396d42f19875330f97333c0488
Danny Gleckler <daniel.gleckler@d2l.com>
2023-06-13 20:14:38 -0400

    Add Windows support

--- bin/migrate-goldens.js
+++ bin/migrate-goldens.js
@@ -1,25 +1,26 @@
 #!/usr/bin/env node
 import { access, mkdir, rename } from 'node:fs/promises';
+import { join, normalize, sep } from 'path';
 import { glob } from 'glob';
 import commandLineArgs from 'command-line-args';
 
-const { pattern = './test/**' } = commandLineArgs({ name: 'pattern', type: String, defaultOption: true });
-const oldSuffix = '/screenshots/ci/golden';
-const newSuffix = '/vdiff/ci/chromium/golden';
+let { pattern = './test/**' } = commandLineArgs({ name: 'pattern', type: String, defaultOption: true });
+pattern = pattern.replace(/^'|'$/g, '');
 
-const dirs = await glob(`${pattern}${oldSuffix}`, { ignore: 'node_modules/**' });
-
+const oldSuffix = 'screenshots/ci/golden';
+const newSuffix = 'vdiff/ci/chromium/golden';
+const dirs = await glob(`${pattern}/${oldSuffix}`, { ignore: 'node_modules/**' });
 const files = [];
 
 await Promise.all(dirs.map(async dir => {
-	files.push(...(await glob(`${dir}/*/*.png`)));
+	files.push(...(await glob(`${dir.replaceAll(sep, '/')}/*/*.png`)));
 
-	const base = dir.replace(oldSuffix, '');
+	const base = dir.replace(normalize(oldSuffix), '');
 
-	await access(`${base}/vdiff`).catch(async err => err && await mkdir(`${base}/vdiff`));
-	await access(`${base}/vdiff/ci`).catch(async err => err && await mkdir(`${base}/vdiff/ci`));
-	await access(`${base}/vdiff/ci/chromium`).catch(async err => err && await mkdir(`${base}/vdiff/ci/chromium`));
-	return rename(dir, `${base}/${newSuffix}`);
+	await access(join(base, 'vdiff')).catch(async err => err && await mkdir(join(base, 'vdiff')));
+	await access(join(base, 'vdiff', 'ci')).catch(async err => err && await mkdir(join(base, 'vdiff', 'ci')));
+	await access(join(base, 'vdiff', 'ci', 'chromium')).catch(async err => err && await mkdir(join(base, 'vdiff', 'ci', 'chromium')));
+	return rename(dir, join(base, normalize(newSuffix)));
 }));
 
 console.log(`Migrated ${files.length} goldens found in ${dirs.length} test directories`);
