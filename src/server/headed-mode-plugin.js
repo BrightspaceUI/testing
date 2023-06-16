82e489ba6b34d7a07266e54f44da6dbceded528c
Danny Gleckler <daniel.gleckler@d2l.com>
2023-06-16 10:01:05 -0400

    Whoopsies. Add renamed plugin

---
+++ src/server/headed-mode-plugin.js
@@ -0,0 +1,16 @@
+import { globSync } from 'glob';
+
+export function headedMode({ manual, watch, pattern }) {
+
+	const files = globSync(pattern, { ignore: './node_modules' });
+
+	return {
+		name: 'brightspace-manual-pause',
+		async transform(context) {
+			if ((watch || manual) && files.includes(context.path.slice(1))) {
+				watch && await new Promise(r => setTimeout(r, 2000));
+				return `debugger;\n${context.body}`;
+			}
+		}
+	};
+}
