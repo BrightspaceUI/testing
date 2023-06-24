f62aa1aa0274f1ff86e18983046acc6aea457ccb
Danny Gleckler <daniel.gleckler@d2l.com>
2023-06-23 21:28:10 -0400

    Recreate golden with this before each test

--- src/browser/vdiff.js
+++ src/browser/vdiff.js
@@ -1,10 +1,19 @@
 import { chai, expect } from '@open-wc/testing';
 import { executeServerCommand } from '@web/test-runner-commands';
 
-chai.Assertion.addMethod('golden', ScreenshotAndCompare);
+mocha.setup({ // eslint-disable-line no-undef
+	rootHooks: {
+		beforeEach() {
+			chai.Assertion.addMethod('golden',
+				(test => function(...args) {
+					return ScreenshotAndCompare.call(test, this._obj, ...args); // eslint-disable-line no-invalid-this
+				})(this.currentTest));
+		}
+	}
+});
 
-async function ScreenshotAndCompare(name, opts) {
-	const elem = this._obj;
+async function ScreenshotAndCompare(elem, opts) {
+	const name = this.fullTitle();
 	const rect = elem.getBoundingClientRect();
 	const { pass, message } = await executeServerCommand('brightspace-visual-diff', { name, rect, opts });
 	if (!pass) {
