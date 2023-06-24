5b6b6a87d1a730bc460e669eacd94d129f5a83e8
Danny Gleckler <daniel.gleckler@d2l.com>
2023-06-24 09:50:12 -0400

    Put test and elem in this

--- src/browser/vdiff.js
+++ src/browser/vdiff.js
@@ -6,15 +6,15 @@ mocha.setup({ // eslint-disable-line no-undef
 		beforeEach() {
 			const { currentTest } = this;
 			chai.Assertion.addMethod('golden', function(...args) {
-				return ScreenshotAndCompare(currentTest, this._obj, ...args); // eslint-disable-line no-invalid-this
+				return ScreenshotAndCompare.call({ test: currentTest, elem: this._obj }, ...args); // eslint-disable-line no-invalid-this
 			});
 		}
 	}
 });
 
-async function ScreenshotAndCompare(test, elem, opts) {
-	const name = test.fullTitle();
-	const rect = elem.getBoundingClientRect();
+async function ScreenshotAndCompare(opts) {
+	const name = this.test.fullTitle();
+	const rect = this.elem.getBoundingClientRect();
 	const { pass, message } = await executeServerCommand('brightspace-visual-diff', { name, rect, opts });
 	if (!pass) {
 		expect.fail(message);
