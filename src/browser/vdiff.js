8906eb790373147f5172273a7ce99501549b5b40
Danny Gleckler <daniel.gleckler@gmail.com>
2023-06-26 13:35:51 -0400

    fix: Recreate golden with this before each test (#41)
    * Recreate golden with this before each test
    
    * Remove unnecessary closure
    
    * Clean up command opts
    
    * Put test and elem in this
    
    * Avoid setting assertion method multiple times

--- src/browser/vdiff.js
+++ src/browser/vdiff.js
@@ -1,11 +1,22 @@
 import { chai, expect } from '@open-wc/testing';
 import { executeServerCommand } from '@web/test-runner-commands';
 
-chai.Assertion.addMethod('golden', ScreenshotAndCompare);
+let test;
 
-async function ScreenshotAndCompare(name, opts) {
-	const elem = this._obj;
-	const rect = elem.getBoundingClientRect();
+chai.Assertion.addMethod('golden', function(...args) {
+	return ScreenshotAndCompare.call({ test, elem: this._obj }, ...args); // eslint-disable-line no-invalid-this
+});
+mocha.setup({ // eslint-disable-line no-undef
+	rootHooks: {
+		beforeEach() {
+			test = this.currentTest;
+		}
+	}
+});
+
+async function ScreenshotAndCompare(opts) {
+	const name = this.test.fullTitle();
+	const rect = this.elem.getBoundingClientRect();
 	const { pass, message } = await executeServerCommand('brightspace-visual-diff', { name, rect, opts });
 	if (!pass) {
 		expect.fail(message);
