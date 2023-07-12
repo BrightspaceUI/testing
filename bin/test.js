65a67c7d29d7aacde49437093af813efcdab409b
Danny Gleckler <daniel.gleckler@d2l.com>
2023-07-12 00:49:08 -0400

    Add --help details

--- bin/test.js
+++ bin/test.js
@@ -10,20 +10,94 @@ const DISALLOWED_OPTIONS = ['--browsers', '--playwright', '--puppeteer', '--grou
 
 const optionDefinitions = [
 	// @web/test-runner options
-	{ name: 'files', type: String, multiple: true },
-	{ name: 'group', type: String, defaultOption: true },
-	{ name: 'manual', type: Boolean },
-	{ name: 'watch', type: Boolean },
-	// custom options
-	{ name: 'chrome', type: Boolean },
-	{ name: 'config', alias: 'c', type: String }, // disabled for wtr
-	{ name: 'filter', alias: 'f', type: String, multiple: true },
-	{ name: 'firefox', type: Boolean },
-	{ name: 'golden', type: Boolean },
-	{ name: 'grep', alias: 'g', type: String },
-	{ name: 'help', alias: 'h', type: Boolean },
-	{ name: 'safari', type: Boolean },
-	{ name: 'timeout', alias: 't', type: Number },
+	{
+		name: 'files',
+		type: String,
+		multiple: true,
+		description: 'Test files to run. Path or glob.\n[Default: ./test/**/*.<type>.js]',
+		order: 8
+	},
+	{
+		name: 'group',
+		type: String,
+		required: true,
+		defaultOption: true,
+		description: 'Name of the group to run tests for\n[Default: unit]',
+		order: 1
+	},
+	{
+		name: 'manual',
+		type: Boolean,
+		description: 'Starts test runner in manual testing mode. Ignores browser options and prints manual testing URL.\n{underline Not compatible with automated browser interactions}\nConsider using --watch to debug in the browser instead',
+		order: 11
+	},
+	{
+		name: 'watch',
+		type: Boolean,
+		description: 'Reload tests on file changes. Allows debugging in all browsers.',
+		order: 9
+	},
+
+	// d2l-test options
+	{
+		name: 'chrome',
+		type: Boolean,
+		description: 'Run tests in Chromium',
+		order: 2
+	},
+	{
+		name: 'config',
+		alias: 'c',
+		type: String,
+		description: 'Location to read config file from\n[Default: ./d2l-test.config.js]',
+		order: 9
+	},
+	{
+		name: 'filter',
+		alias: 'f',
+		type: String,
+		multiple: true,
+		description: 'Filter test files by replacing wildcards with this glob',
+		order: 6
+	},
+	{
+		name: 'firefox',
+		type: Boolean,
+		description: 'Run tests in Firefox',
+		order: 3
+	},
+	{
+		name: 'golden',
+		type: Boolean,
+		description: 'Generate new golden screenshots',
+		order: 10
+	},
+	{
+		name: 'grep',
+		alias: 'g',
+		type: String,
+		description: 'Only run tests matching this string or regexp',
+		order: 7
+	},
+	{
+		name: 'help',
+		type: Boolean,
+		description: 'Print usage information and exit',
+		order: 12
+	},
+	{
+		name: 'safari',
+		type: Boolean,
+		description: 'Run tests in Webkit',
+		order: 4
+	},
+	{
+		name: 'timeout',
+		alias: 't',
+		type: Number,
+		description: 'Test timeout threshold in ms\n[Default: 2000]',
+		order: 5
+	},
 ];
 
 const cliArgs = commandLineArgs(optionDefinitions, { partial: true });
@@ -36,11 +110,13 @@ if (cliArgs.help) {
 		},
 		{
 			header: 'Usage',
-			content: 'd2l-test <group> [options]',
+			content: 'd2l-test [options]',
 		},
 		{
 			header: 'Options',
 			optionList: optionDefinitions
+				.map(o => (o.description += '\n') && o)
+				.sort((a, b) => (a.order > b.order ? 1 : -1))
 		}
 	]);
 	process.stdout.write(help);
