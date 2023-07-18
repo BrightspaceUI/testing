6fa4680c9f0c536eb4cbb85d9180bbbe205cc674
Danny Gleckler <daniel.gleckler@d2l.com>
2023-07-18 08:04:19 -0400

    Remove page load delay

--- src/server/headed-mode-plugin.js
+++ src/server/headed-mode-plugin.js
@@ -5,14 +5,11 @@ export function headedMode({ open, watch, pattern }) {
 
 	const files = globSync(pattern, { ignore: 'node_modules/**', posix: true });
 
-	const delay = ms => ms && new Promise(r => setTimeout(r, ms));
-
 	return {
 		name: 'brightspace-headed-mode',
 		async transform(context) {
 
 			if ((watch || open) && files.includes(context.path.slice(1))) {
-				await delay(0);
 				const pausePath = join(dirname(import.meta.url), 'pause.js').replace('file:', '');
 				return `debugger;\nimport '${pausePath}'\n${context.body}`;
 			}
