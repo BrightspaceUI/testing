7bd0fbb0d1936e5ea277f25fd4e493a8b3efbb5e
Danny Gleckler <daniel.gleckler@d2l.com>
2023-07-24 13:04:28 -0400

    Fix indentation

--- src/server/pause.js
+++ src/server/pause.js
@@ -2,86 +2,86 @@ const test = window.d2lTest = {};
 test.pause = new Promise(r => test.start = r);
 
 const controls = `
-       <style>
-               #d2l-test-controls [hidden] {
-                       display: none !important;
-               }
+	<style>
+		#d2l-test-controls [hidden] {
+			display: none !important;
+		}
 
-               #d2l-test-controls #start, #run {
-                       display: flex;
-                       gap: 1em;
-                       font-size: 18px;
-                       font-family: 'Lato', sans-serif;
-                       padding: 1em;
-                       background: #fff;
-                       color: #222;
-                       margin: calc(-30px + -8px) calc(-30px + -8px) calc(30px + 8px);
-                       flex-wrap: wrap;
-                       align-items: center;
-                       box-shadow: 0 0 5px rgba(0,0,0,.5);
-               }
+		#d2l-test-controls #start, #run {
+			display: flex;
+			gap: 1em;
+			font-size: 18px;
+			font-family: 'Lato', sans-serif;
+			padding: 1em;
+			background: #fff;
+			color: #222;
+			margin: calc(-30px + -8px) calc(-30px + -8px) calc(30px + 8px);
+			flex-wrap: wrap;
+			align-items: center;
+			box-shadow: 0 0 5px rgba(0,0,0,.5);
+		}
 
-               #d2l-test-controls #start {
-                       font-size: 24px;
-                       height: 600px;
-                       flex-direction: column;
-                       justify-content: center;
-                       margin-bottom: 0;
-                       box-shadow: none;
-               }
+		#d2l-test-controls #start {
+			font-size: 24px;
+			height: 600px;
+			flex-direction: column;
+			justify-content: center;
+			margin-bottom: 0;
+			box-shadow: none;
+		}
 
-               #d2l-test-controls #test-name {
-                       flex: 1;
-               }
+		#d2l-test-controls #test-name {
+			flex: 1;
+		}
 
-               #d2l-test-controls button {
-                       font-family: 'Lato', sans-serif;
-                       background-color: #e3e9f1;
-                       color: #202122;
-                       border-radius: .3em;
-                       border-style: none;
-                       font-weight: 700;
-                       font-size: .9em;
-                       margin: 0;
-                       min-height: calc(2em + 2px);
-                       outline: none;
-                       padding: .55em 1.5em;
-                       text-align: center;
-                       line-height: 1em;
-               }
+		#d2l-test-controls button {
+			font-family: 'Lato', sans-serif;
+			background-color: #e3e9f1;
+			color: #202122;
+			border-radius: .3em;
+			border-style: none;
+			font-weight: 700;
+			font-size: .9em;
+			margin: 0;
+			min-height: calc(2em + 2px);
+			outline: none;
+			padding: .55em 1.5em;
+			text-align: center;
+			line-height: 1em;
+			cursor: pointer;
+		}
 
-               #d2l-test-controls button.primary {
-                       color: #fff;
-                       background-color: #006fbf;
-               }
+		#d2l-test-controls button:focus {
+			background-color: #cdd5dc;
+		}
 
-               #d2l-test-controls button:focus {
-                       background-color: #cdd5dc;
-               }
+		#d2l-test-controls button:focus {
+			background-color: #cdd5dc;
+		}
 
-               #d2l-test-controls button:focus-visible {
-                       box-shadow: 0 0 0 2px #fff, 0 0 0 4px #006fbf;
-               }
+		#d2l-test-controls button:focus-visible {
+			box-shadow: 0 0 0 2px #fff, 0 0 0 4px #006fbf;
+		}
 
-               #d2l-test-controls button.primary:focus {
-                       background-color: #004489;
-               }
+		#d2l-test-controls button.primary:focus {
+			background-color: #004489;
+		}
 
-               #d2l-test-controls button[disabled] {
-                       opacity: 0.5;
-               }
-       </style>
-       <div id="d2l-test-controls">
-               <div id="start">
-                       <span>.${window.__WTR_CONFIG__.testFile.split('?')[0]}</span>
-                       <button id="start-button" class="primary">Start</button>
-               </div>
-               <div id="run" hidden>
-                       <button id="run-button" class="primary">Run</button>
-                       <span id="test-name"></span>
-                       <button id="run-all-button">Run All</button>
-               </div>
-       </div>
+		#d2l-test-controls button[disabled] {
+			opacity: 0.5;
+		}
+	</style>
+	<div id="d2l-test-controls">
+		<div id="start">
+			<span>.${window.__WTR_CONFIG__.testFile.split('?')[0]}</span>
+			<button id="start-button" class="primary">Start</button>
+		</div>
+		<div id="run" hidden>
+			<button id="run-button" class="primary">Run</button>
+			<span id="test-name"></span>
+			<button id="run-all-button">Run All</button>
+		</div>
+	</div>
 `;
 
 document.body.insertAdjacentHTML('afterBegin', controls);
