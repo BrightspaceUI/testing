ab988d95a57de89d1c2c708b0b394f52b003b017
Danny Gleckler <daniel.gleckler@gmail.com>
2023-07-26 14:34:58 -0400

    feat: Fix Firefox screenshots (#95)
    * Fix firefox screenshots
    
    * Swap map to forEach

--- src/browser/vdiff.js
+++ src/browser/vdiff.js
@@ -1,6 +1,9 @@
 import { chai, expect } from '@open-wc/testing';
 import { executeServerCommand } from '@web/test-runner-commands';
 
+// start loading fonts early
+[...document.fonts].forEach(font => font.load());
+
 let test;
 
 /* eslint-disable no-undef, no-invalid-this */
