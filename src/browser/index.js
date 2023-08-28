8ff05a2d3eedb618ca583cb5aafd82c2b766c36f
Stacey Van Herk <13419300+svanherk@users.noreply.github.com>
2023-08-28 16:08:27 -0400

    Sort exports
    Co-authored-by: Dave Lockhart <dlockhart@users.noreply.github.com>

--- src/browser/index.js
+++ src/browser/index.js
@@ -1,5 +1,5 @@
 import './vdiff.js';
 
-export { assert, aTimeout, defineCE, expect, html, nextFrame, oneEvent, oneDefaultPreventedEvent, waitUntil } from '@open-wc/testing';
+export { assert, aTimeout, defineCE, expect, html, nextFrame, oneDefaultPreventedEvent, oneEvent, waitUntil } from '@open-wc/testing';
 export { clickAt, clickElem, focusElem, hoverAt, hoverElem, sendKeys, sendKeysElem } from './commands.js';
 export { fixture } from './fixture.js';
