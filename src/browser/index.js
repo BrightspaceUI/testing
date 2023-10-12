b49bfb754e84f19a61737d7b848cef96264b5e7f
Dave Lockhart <dlockhart@users.noreply.github.com>
2023-10-12 14:32:08 -0400

    feat: add runConstructor

--- src/browser/index.js
+++ src/browser/index.js
@@ -3,3 +3,4 @@ import './vdiff.js';
 export { assert, aTimeout, defineCE, expect, html, nextFrame, oneDefaultPreventedEvent, oneEvent, waitUntil } from '@open-wc/testing';
 export { clickAt, clickElem, focusElem, hoverAt, hoverElem, sendKeys, sendKeysElem } from './commands.js';
 export { fixture } from './fixture.js';
+export { runConstructor } from './constructor.js';
---
+++ test/browser/constructor.test.js
@@ -0,0 +1,26 @@
+import { defineCE, expect, oneEvent, runConstructor } from '../../src/browser/index.js';
+
+describe('constructor', () => {
+
+	it('should fail if element is not defined', () => {
+		expect(() => runConstructor('not-defined'))
+			.to.throw('expected undefined not to be undefined');
+	});
+
+	it('should fail if element\'s constructor throws', async() => {
+		const tag = defineCE(class extends HTMLElement {
+			constructor() {
+				super();
+				throw new Error('expected error');
+			}
+		});
+		setTimeout(() => runConstructor(tag));
+		await oneEvent(window, 'd2l-test-runner-expected-error');
+	});
+
+	it('should pass otherwise', () => {
+		const tag = defineCE(class extends HTMLElement {});
+		runConstructor(tag);
+	});
+
+});
