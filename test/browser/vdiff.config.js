1c735000cfed071dacaafa7d7daec751735f2585
Stacey Van Herk <13419300+svanherk@users.noreply.github.com>
2023-10-23 13:39:42 -0400

    feat: US158385 - Fail when pixel diff is 0 but byte size has changed (#223)
    * US158385 - If the diff is 0 pixels, check that the byte size between the golden and the screenshot are the same, fail if not and surface in the report
    
    * Add test case
    
    * Temporarily turn on golden mode
    
    * Force failure
    
    * Updating vdiff goldens (#225)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Put test back, but allow it to fail as it would in real life
    
    * Put test back
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/browser/vdiff.config.js
+++ test/browser/vdiff.config.js
@@ -1,16 +1,46 @@
-import process from 'node:process';
+import { argv, env } from 'node:process';
+import { dirname, join } from 'node:path';
+import { readFile, writeFile } from 'node:fs/promises';
+import { PATHS } from '../../src/server/visual-diff-plugin.js';
+import { PNG } from 'pngjs';
+
+const isCI = !!env['CI'];
 
 function getGoldenFlag() {
 	return {
 		name: 'vdiff-get-golden-flag',
 		async executeCommand({ command }) {
 			if (command !== 'vdiff-get-golden-flag') return;
-			return process.argv.indexOf('--golden') > -1;
+			return argv.indexOf('--golden') > -1;
+		}
+	};
+}
+
+function modifyGolden() {
+	let rootDir;
+	return {
+		name: 'vdiff-modify-golden-file',
+		async serverStart({ config }) {
+			rootDir = config.rootDir;
+		},
+		async executeCommand({ command, payload, session }) {
+			if (command !== 'vdiff-modify-golden-file') return;
+			const browser = session.browser.name.toLowerCase();
+			const testPath = dirname(session.testFile).replace(rootDir, '');
+			const filePath = join(rootDir, PATHS.VDIFF_ROOT, testPath, payload.testCategory);
+			const fileLocation = isCI ? PATHS.FAIL : PATHS.GOLDEN;
+
+			const fullPath = join(filePath, fileLocation, browser, payload.fileName);
+			const data = await readFile(fullPath);
+			const png = PNG.sync.read(data);
+			const buffer = PNG.sync.write(png, { inputHasAlpha: false });
+			await writeFile(fullPath, buffer);
+			return true;
 		}
 	};
 }
 
 export default {
 	pattern: () => 'test/browser/**/*.vdiff.js',
-	plugins: [getGoldenFlag()]
+	plugins: [getGoldenFlag(), modifyGolden()]
 };
