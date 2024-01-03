46f58884eb3cfc859055f1340b0099353a77e901
Stacey Van Herk <13419300+svanherk@users.noreply.github.com>
2024-01-03 10:22:35 -0500

    fix: Fix rollup.config.js for Windows (#283)

--- src/server/rollup.config.js
+++ src/server/rollup.config.js
@@ -1,10 +1,11 @@
+import { cwd } from 'node:process';
 import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
 import { join } from 'path';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { PATHS } from './visual-diff-plugin.js';
 
 export default {
-	input: join(PATHS.VDIFF_ROOT, PATHS.REPORT_ROOT, './temp/index.html'),
+	input: join(cwd(), PATHS.VDIFF_ROOT, PATHS.REPORT_ROOT, './temp/index.html'),
 	output: {
 		dir: join(PATHS.VDIFF_ROOT, PATHS.REPORT_ROOT)
 	},
