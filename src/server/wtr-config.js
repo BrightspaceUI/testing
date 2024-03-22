83de73c4bdeff7494aeb0238de87cc07e95d4e68
Stacey Van Herk <13419300+svanherk@users.noreply.github.com>
2024-03-22 10:43:36 -0400

    feat: GAUD-6130 - Don't replace set testsFinishTimeout value from config (#347)

--- src/server/wtr-config.js
+++ src/server/wtr-config.js
@@ -219,7 +219,7 @@ export class WTRConfig {
 				files: this.#pattern
 			});
 		} else if (group === 'vdiff') {
-			config.testsFinishTimeout = 5 * 60 * 1000;
+			if (!config.testsFinishTimeout) config.testsFinishTimeout = 5 * 60 * 1000;
 
 			config.reporters ??= [ defaultReporter() ];
 			config.reporters.push(visualDiffReporter({ updateGoldens: golden }));
