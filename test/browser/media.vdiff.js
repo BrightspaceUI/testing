281a20d2235373bc32b844a1ed454385423b445c
Dave Lockhart <dlockhart@users.noreply.github.com>
2024-09-23 12:36:19 -0400

    slow test

--- test/browser/media.vdiff.js
+++ test/browser/media.vdiff.js
@@ -23,6 +23,12 @@ const mediaTag = defineCE(
 
 describe('media', () => {
 
+	it('default', async() => {
+		const elem = await fixture(`<${mediaTag}></${mediaTag}>`);
+		await new Promise(resolve => setTimeout(resolve, 20000));
+		await expect(elem).to.be.golden();
+	});
+
 	['screen', 'print'].forEach(media => {
 		it(media, async() => {
 			const elem = await fixture(`<${mediaTag}></${mediaTag}>`, { media });
