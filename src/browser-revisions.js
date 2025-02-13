74ac57611017051e6ef110ed2ed6cacd4be90ded
Danny Gleckler <daniel.gleckler@d2l.com>
2025-02-12 20:59:40 -0500

    Remove partial version entries

--- src/browser-revisions.js
+++ src/browser-revisions.js
@@ -1,26 +1,6 @@
 export default [
 	{
 		"name": "chromium",
-		"revision": "1071",
-		"version": "115"
-	},
-	{
-		"name": "chromium",
-		"revision": "1140",
-		"version": "130"
-	},
-	{
-		"name": "webkit",
-		"revision": "2083",
-		"version": "18"
-	},
-	{
-		"name": "firefox",
-		"revision": "1465",
-		"version": "131"
-	},
-	{
-		"name": "chromium",
 		"revision": "1155",
 		"version": "133.0.6943.16"
 	},
