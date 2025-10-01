9cf93aca36757b0311b2a3f216df433a4555f2be
Brennan D'Aguilar <brennan.daguilar@gmail.com>
2025-10-01 16:25:17 -0400

    fix: Use Playwright locale setting instead of env.LANG (#783)
    * Use Playwright locale setting instead of env.LANG
    
    On windows, tests run in chromium are using the system region settings for the browser's language.  Firefox and webkit are always using "en-us" even without the LANG env var set.
    
    The LANG env variable doesn't affect chromium on windows systems (https://peter.sh/experiments/chromium-command-line-switches/#lang) so this switches to using playwright's `locale` setting (https://playwright.dev/docs/api/class-browsertype#browser-type-launch-persistent-context-option-locale) which does correctly set chromium's language on windows.
    
    * Add a test to verify the playwright locale setting

--- test/browser/environment.test.js
+++ test/browser/environment.test.js
@@ -6,4 +6,10 @@ describe('environment', () => {
 		expect(window.isD2LTestPage).to.be.true;
 	});
 
+	it('should default to "en-us" locale', async() => {
+		// Firefox always returns the region portion in uppercase
+		// https://developer.mozilla.org/en-US/docs/Web/API/NavigatorLanguage/language
+		expect(navigator.language.toLowerCase()).to.equal('en-us');
+	});
+
 });
